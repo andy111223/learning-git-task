6caeb1d8103ac872ada3f182790b70016cc5986b
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2024-09-24 10:03:30 +0200

    Add dictionary and loop

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("This is a git learning exercise")
+shoppingList = {
+    "grocery store":['eggs','tomato','butter'],
+    "convenience store":['newspaper','Coca Cola','chewing gum'],
+    "pet store":['catfood','dogfood','birdfood']
+    }
+total_products = 0
+
+for shop, items in shoppingList.items():
+    capitalized_items = [item.capitalize() for item in items]
+    total_products += len(items)
+    formatted_items = ', '.join(capitalized_items) 
+    print(f"Idę do {shop.capitalize()} i kupuję tam {formatted_items}")
+print(f"W sumie kupuję {total_products} produktów")
